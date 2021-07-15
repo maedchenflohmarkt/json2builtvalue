dff89c8fe2aa810a90dbd6a6a503c50a15f6a836
Siarhei Alikhver <solikhver@users.noreply.github.com>
2021-07-15 13:14:55 +0200

    fix issues related to lint checks

--- lib/generator.dart
+++ lib/generator.dart
@@ -68,7 +68,7 @@ class Generator {
     String classString = topLevelClass.accept(new DartEmitter()).toString();
 
     String header = """
-      ${_buildNestedImports(genClass.fields, options)}
+      ${_buildNestedImports(genClass, options)}
       
       part '${new ReCase(genClass.name).snakeCase}.g.dart';
     
@@ -84,13 +84,14 @@ class Generator {
       new ReCase(name).pascalCase;
 
   static String _buildNestedImports(
-      List<GenField> fields, SerializationOptions? options) {
+      GenClass genClass, SerializationOptions? options) {
     String baseImports = """
+      // DO NOT EDIT. This is code generated via package:json2builtvalue
       import 'dart:convert';
       import 'package:built_value/built_value.dart';
       import 'package:built_value/serializer.dart';
     """;
-    baseImports += fields.any((GenField field) =>
+    baseImports += genClass.fields.any((GenField field) =>
             field.type.type == JsonType.LIST ||
             field.type.type == JsonType.LIST_N)
         ? """
@@ -102,15 +103,22 @@ class Generator {
       import 'package:${options.serializersImport}';
     """;
     }
-    List items = fields
+
+    List<String> items = genClass.fields
+        // exclude class name
+        .where((element) => (genClass.name.compareTo(element.type.name) != 0))
         .where((GenField field) =>
             field.type.type == JsonType.MAP ||
             field.type.listType == JsonType.MAP ||
             field.type.type == JsonType.MAP_N ||
             field.type.listType == JsonType.MAP_N)
         .map((GenField field) => ReCase(field.type.name).snakeCase)
-        .map((String name) => "import '$name.dart';")
-        .toList();
+        //avoid duplicate imports
+        .toSet()
+        .toList()
+          //sort alphabetically
+          ..sort();
+    items = items.map((String name) => "import '$name.dart';").toList();
 
     String generatedClasses =
         items.isNotEmpty ? items.reduce((s1, s2) => "$s1\n$s2") : "";
